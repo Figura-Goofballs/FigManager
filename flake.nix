337fcbd310ab0ee56ba55a37c4107699dce4e3bf
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-10-28 15:01:02 -0400

    rename binary

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	inputs.fia.url = github:poollovernathan/fia;
 	outputs = inputs: rec {
 		packages = inputs.fia.lib.perSystem (pkgs: {
-			default = pkgs.writers.writeBashBin "fig-manager" ''
+			default = pkgs.writers.writeBashBin "figman" ''
 				export PATH=${pkgs.lib.escapeShellArg (pkgs.lib.makeBinPath [
 					pkgs.bash
 					pkgs.coreutils-full
