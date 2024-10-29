4cbc9b21ada3045242511a21419c2532373305a1
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-10-29 10:04:12 -0400

    format flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,28 @@
 {
-	inputs = {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-	outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs{inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs.lua54Packages; [
-          lua
-          dkjson # JSON
-          luasocket # HTTP
-          luasec
-          luafilesystem # FS
-        ];
-      };
-	  }
-  );
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs.lua54Packages; [
+            lua
+            dkjson # JSON
+            luasocket # HTTP
+            luasec
+            luafilesystem # FS
+          ];
+        };
+      }
+    );
 }
