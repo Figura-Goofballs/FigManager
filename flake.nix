ed745e314d513d1ff8ebe44e73b59cff27f9feef
TheBunnyMan123 <bunny@tkbunny.net>
2024-10-30 21:16:53 -0500

    flake stuff and remove files

--- flake.nix
+++ flake.nix
@@ -4,10 +4,44 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-	outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs{inherit system;};
-    in {
+	outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages = {
+        default = pkgs.stdenv.mkDerivation rec {
+          name = "figman";
+          src = ./src;
+
+          buildInputs = with pkgs; [
+            bash
+
+            (lua54Packages.lua.withPackages (ps: with ps; [
+              dkjson
+              luasocket
+              luasec
+              luafilesystem
+            ]))
+          ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp "${pkgs.writeShellScript "figman.sh" ''
+              LUA_PATH="${src}/?.lua;$LUA_PATH" lua ${src}/main.lua "$@"
+            ''}" $out/bin/figman
+
+            wrapProgram $out/bin/figman \
+              --prefix PATH : ${pkgs.lib.makeBinPath buildInputs}\
+          '';
+        };
+      };
+
+      apps = rec {
+        figman = flake-utils.lib.mkApp { drv = packages.default; };
+        default = figman;
+      };
+
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs.lua54Packages; [
           lua
